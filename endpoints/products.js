89315f070fbe9987c36c073c8190fb167b11390b
Eric <erichansson.united@gmail.com>
2025-07-17 17:10:06 -0400

    Starting to work on REST API for the backend to be able have a working web app that has products being pulled from a database and not from the frontend

---
+++ endpoints/products.js
@@ -0,0 +1,25 @@
+const db = require("../db/db");
+
+module.exports = function(app){
+
+    // Get the products from data base and send it back to client
+    app.get("/products", async(req,res)=>{
+        try{
+            const result = await db.pool.query("Select * from products");
+            res.send(result);
+        }  
+        catch(error){
+            throw error;
+        }
+    });
+
+    // Get a specific product and it's details for inspection of one item
+    app.get("/product", async(req,res)=>{
+        try{
+
+        }
+        catch(error){
+            throw error;
+        }
+    });
+}
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
-
 const port = 3000;
+const bodyParser = require("body-parser");
+//require("dotenv").config()
+
+console.log(process.env)
+app.use(bodyParser.json());
+
 
 app.get("/", (req,res) =>{
     res.send("Hello World!");
 })
 
 app.listen(port, () =>{
-    console.log("Starting server on port ${port}");
+    console.log(`Starting server on port ${port}`);
 })
