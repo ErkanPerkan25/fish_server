eb0e1b345ba4346eab080101d81a803d5b2ff706
Eric <erichansson.united@gmail.com>
2025-07-24 15:20:09 -0400

    I have now so the API can call and get data from the database based on the checked boxes on the frontend but it can only take on value from the specific column while it needs to do it for multiple values.

--- endpoints/products.js
+++ endpoints/products.js
@@ -5,10 +5,23 @@ module.exports = function(app){
     // Get the products from data base and send it back to client
     app.get("/products", async(req,res)=>{
         try{
-            const {type, brand} = req.query;
-            console.log(type + "\n" + brand);
-            const result = await db.pool.query("Select * from product");
-            res.send(result);
+            // Checks if the paramers are empty or not
+            const emptyItems = Object.values(req.query).every(
+                value => value === '' || value === null || value === undefined
+            );
+
+            if(emptyItems){
+                const result = await db.pool.query("SELECT * FROM product");
+                res.send(result);
+            }
+
+            else{
+                const type = req.query.type;
+                console.log(type);
+                //const brand = req.query.brand;
+                const data = await db.pool.query(`SELECT * from product WHERE Basic_unit IN ('${type}')`);
+                res.send(data);
+            }
         }  
         catch(error){
             throw error;
